801118d3eb7d57dcc75e740cadc634c3d8bc4766
golfzakrub <70587016+golfzakrub@users.noreply.github.com>
2022-03-27 21:15:49 +0700

    Add table in GUI

---
+++ data.py
@@ -0,0 +1,24 @@
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtCore import  QSortFilterProxyModel
+
+import pandas as pd
+from pandasModel import pandasModel 
+
+class dataManager():
+
+
+    def getFile(self):
+
+        self.filename = QFileDialog.getOpenFileName(filter = "Excel or CSV(*.csv ,*.xls ,*.xlsx ,*.xlsm)")[0]
+        if self.filename == "" :
+            print("please select file")
+        else:
+            return self.filename
+
+    def readData(self,filename):
+        self.all_data = pd.read_csv(filename,encoding = 'windows-1252').dropna()
+        print("read file finished")
+        return self.all_data
+
